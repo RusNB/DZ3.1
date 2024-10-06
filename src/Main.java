dc3c9330f7f41034190d869736d98907a6be3c6c
RusNB <balushkin-007@mail.ru>
2024-10-06 20:59:47 +0300

    chenge kod

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
         int miles = service.calculate(price); // должно получиться 500
+
+
         System.out.println(miles);
     }
 }
